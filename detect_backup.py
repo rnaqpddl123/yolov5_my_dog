679d05d27f5f1207325d45d82bfe8c80bd4bc275
rnaqpddl123 <83867157+rnaqpddl123@users.noreply.github.com>
2021-11-01 17:26:35 +0900

    update
    best.pt , detect.py update

--- detect_backup.py
+++ detect_backup.py
@@ -11,11 +11,18 @@ import sys
 import time
 from pathlib import Path
 
+import datetime
+import os
+
 import json
 import cv2
 import numpy as np
 import torch
 import torch.backends.cudnn as cudnn
+
+#주영
+from highlight import Highlight
+from json_move import move_dog
 
 FILE = Path(__file__).absolute()
 sys.path.append(FILE.parents[0].as_posix())  # add yolov5/ to path
@@ -29,8 +36,8 @@ from utils.torch_utils import select_device, load_classifier, time_sync
 
 
 @torch.no_grad()
-def run(weights='yolov5s.pt',  # model.pt path(s)
-        source='data/videos',  # file/dir/URL/glob, 0 for webcam
+def run(weights='best.pt',  # model.pt path(s)
+        source='data/videos/dog_sample.mp4',  # file/dir/URL/glob, 0 for webcam
         imgsz=640,  # inference size (pixels)
         conf_thres=0.25,  # confidence threshold
         iou_thres=0.45,  # NMS IOU threshold
@@ -57,7 +64,6 @@ def run(weights='yolov5s.pt',  # model.pt path(s)
     save_img = not nosave and not source.endswith('.txt')  # save inference images
     webcam = source.isnumeric() or source.endswith('.txt') or source.lower().startswith(
         ('rtsp://', 'rtmp://', 'http://', 'https://'))
-
     # Directories
     save_dir = increment_path(Path(project) / name, exist_ok=exist_ok)  # increment run
     (save_dir / 'labels' if save_txt else save_dir).mkdir(parents=True, exist_ok=True)  # make dir
@@ -124,163 +130,175 @@ def run(weights='yolov5s.pt',  # model.pt path(s)
     t0 = time.time()
     #주영 json 형식으로 저장할때 시간체크위해서 
     data= {}
-    frame = []
+    data["frames"] = []
+    count1 =0
+    delay_frame = 0
+
 
     for path, img, im0s, vid_cap in dataset:
-        if onnx:
-            img = img.astype('float32')
-        else:
-            img = torch.from_numpy(img).to(device)
-            img = img.half() if half else img.float()  # uint8 to fp16/32
-        img = img / 255.0  # 0 - 255 to 0.0 - 1.0
-        if len(img.shape) == 3:
-            img = img[None]  # expand for batch dim
-
-        # Inference
-        t1 = time_sync()
-        if pt:
-            visualize = increment_path(save_dir / Path(path).stem, mkdir=True) if visualize else False
-            pred = model(img, augment=augment, visualize=visualize)[0]
-        elif onnx:
-            pred = torch.tensor(session.run([session.get_outputs()[0].name], {session.get_inputs()[0].name: img}))
-        else:  # tensorflow model (tflite, pb, saved_model)
-            imn = img.permute(0, 2, 3, 1).cpu().numpy()  # image in numpy
-            if pb:
-                pred = frozen_func(x=tf.constant(imn)).numpy()
-            elif saved_model:
-                pred = model(imn, training=False).numpy()
-            elif tflite:
-                if int8:
-                    scale, zero_point = input_details[0]['quantization']
-                    imn = (imn / scale + zero_point).astype(np.uint8)  # de-scale
-                interpreter.set_tensor(input_details[0]['index'], imn)
-                interpreter.invoke()
-                pred = interpreter.get_tensor(output_details[0]['index'])
-                if int8:
-                    scale, zero_point = output_details[0]['quantization']
-                    pred = (pred.astype(np.float32) - zero_point) * scale  # re-scale
-            pred[..., 0] *= imgsz[1]  # x
-            pred[..., 1] *= imgsz[0]  # y
-            pred[..., 2] *= imgsz[1]  # w
-            pred[..., 3] *= imgsz[0]  # h
-            pred = torch.tensor(pred)
-            
-
-        # NMS
-        pred = non_max_suppression(pred, conf_thres, iou_thres, classes, agnostic_nms, max_det=max_det)
-        t2 = time_sync()
-        det = pred[0]
-        dog_log=[]
-        # 주영 json 형식으로 파일 변환후 찍기(방법2)
-
-        # data["frames"] = [] 
-
-        # counts=0
-        # for i in det:
-        #     counts += 1 
-        #     if i[5] == 1.60000e+01:
-        #         # dog_log.append(i)
-        #         data["frames"].append({
-        #         "time":counts,
-        #         # "x1":float(i[0]),
-        #         # "y1":float(i[1]),
-        #         # "x2":float(i[2]),
-        #         # "y2":float(i[3]),
-        #         "confidence": float(i[4]),
-        #         "center_x": (float(i[0]) + float(i[2])/2),
-        #         "center_y": (float(i[1]) + float(i[3])/2),
-        #         "width":(float(i[2]) - float(i[0])),
-        #         "height":(float(i[3]) - float(i[1]))
-        #         })
-
-        # #주영 json 형식으로 파일 변환후 찍기
-        count = 0
-        for i in det:
-            if i[5] == 1.60000e+01:
-                dog_log.append(i)
-        count +=1
-
-        for i in dog_log:
-            count += 1
-            data[f"time{count}"] = []
-            data[f"time{count}"].append({
-                "x1":float(i[0]),
-                "y1":float(i[1]),
-                "x2":float(i[2]),
-                "y2":float(i[3]),
-                "confidence": float(i[4])
-            })
-
-        
-        # print(data)
-
-        # Second-stage classifier (optional)
-        if classify:
-            pred = apply_classifier(pred, modelc, img, im0s)
-
-        # Process predictions
-        for i, det in enumerate(pred):  # detections per image
-            if webcam:  # batch_size >= 1
-                p, s, im0, frame = path[i], f'{i}: ', im0s[i].copy(), dataset.count
+        delay_frame +=1
+        if delay_frame % 12 == 0:
+            if onnx:
+                img = img.astype('float32')
             else:
-                p, s, im0, frame = path, '', im0s.copy(), getattr(dataset, 'frame', 0)
+                img = torch.from_numpy(img).to(device)
+                img = img.half() if half else img.float()  # uint8 to fp16/32
+            img = img / 255.0  # 0 - 255 to 0.0 - 1.0  
+            if len(img.shape) == 3:
+                img = img[None]  # expand for batch dim
+            # Inference
+            t1 = time_sync()
+            if pt:
+                visualize = increment_path(save_dir / Path(path).stem, mkdir=True) if visualize else False
+                pred = model(img, augment=augment, visualize=visualize)[0]
+            elif onnx:
+                pred = torch.tensor(session.run([session.get_outputs()[0].name], {session.get_inputs()[0].name: img}))
+            else:  # tensorflow model (tflite, pb, saved_model)
+                imn = img.permute(0, 2, 3, 1).cpu().numpy()  # image in numpy
+                if pb:
+                    pred = frozen_func(x=tf.constant(imn)).numpy()
+                elif saved_model:
+                    pred = model(imn, training=False).numpy()
+                elif tflite:
+                    if int8:
+                        scale, zero_point = input_details[0]['quantization']
+                        imn = (imn / scale + zero_point).astype(np.uint8)  # de-scale
+                    interpreter.set_tensor(input_details[0]['index'], imn)
+                    interpreter.invoke()
+                    pred = interpreter.get_tensor(output_details[0]['index'])
+                    if int8:
+                        scale, zero_point = output_details[0]['quantization']
+                        pred = (pred.astype(np.float32) - zero_point) * scale  # re-scale
+                pred[..., 0] *= imgsz[1]  # x
+                pred[..., 1] *= imgsz[0]  # y
+                pred[..., 2] *= imgsz[1]  # w
+                pred[..., 3] *= imgsz[0]  # h
+                pred = torch.tensor(pred)
+                
 
-            p = Path(p)  # to Path
-            save_path = str(save_dir / p.name)  # img.jpg
-            txt_path = str(save_dir / 'labels' / p.stem) + ('' if dataset.mode == 'image' else f'_{frame}')  # img.txt
-            s += '%gx%g ' % img.shape[2:]  # print string
-            gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
-            imc = im0.copy() if save_crop else im0  # for save_crop
-            if len(det):
-                # Rescale boxes from img_size to im0 size
-                det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
+            # NMS
+            pred = non_max_suppression(pred, conf_thres, iou_thres, classes, agnostic_nms, max_det=max_det)
+            t2 = time_sync()
+            det = pred[0]
 
-                # Print results
-                for c in det[:, -1].unique():
-                    n = (det[:, -1] == c).sum()  # detections per class
-                    s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
 
-                # Write results
-                for *xyxy, conf, cls in reversed(det):
-                    if save_txt:  # Write to file
-                        xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
-                        line = (cls, *xywh, conf) if save_conf else (cls, *xywh)  # label format
-                        with open(txt_path + '.txt', 'a') as f:
-                            f.write(('%g ' * len(line)).rstrip() % line + '\n')
+            #주영 json 형식으로 파일 변환후 찍기
+            count1 += 1
+            exist_dog = False
+            frame = []
+            for i in det:
+                c =float(i[4])
+                c_x = (float(i[0]) + float(i[2])/2)
+                c_y = (float(i[1]) + float(i[3])/2)
+                w = (float(i[2]) - float(i[0]))
+                h= (float(i[3]) - float(i[1]))
+                now = datetime.datetime.now()
+                if i[5] == 0.00000:
+                    exist_dog = True
+                    asd = {
+                        "confidence":c,
+                        "center_x":c_x, 
+                        "center_y":c_y,
+                        "width":w,
+                        "height":h
+                    }
+                    frame.append(asd)
+                
+            if exist_dog == True:
+                total={
+                    "frame":frame,
+                    "frame_no":count1
+                }
+                data["frames"].append(total)
+            else:
+                frame =[]
+                total={
+                    "frame":frame,
+                    "frame_no":count1
+                }
+                data["frames"].append(total)
 
-                    if save_img or save_crop or view_img:  # Add bbox to image
-                        c = int(cls)  # integer class
-                        label = None if hide_labels else (names[c] if hide_conf else f'{names[c]} {conf:.2f}')
-                        im0 = plot_one_box(xyxy, im0, label=label, color=colors(c, True), line_width=line_thickness)
-                        if save_crop:
-                            save_one_box(xyxy, imc, file=save_dir / 'crops' / names[c] / f'{p.stem}.jpg', BGR=True)
 
-            # Print time (inference + NMS)
-            print(f'{s}Done. ({t2 - t1:.3f}s)')
 
-            # Stream results
-            if view_img:
-                cv2.imshow(str(p), im0)
-                cv2.waitKey(1)  # 1 millisecond
+                
 
-            # Save results (image with detections) (데이터 저장하고싶으면 아래코드활성화)
-            if save_img:
-                if dataset.mode == 'image':
-                    cv2.imwrite(save_path, im0)
-                else:  # 'video' or 'stream'
-                    if vid_path[i] != save_path:  # new video
-                        vid_path[i] = save_path
-                        if isinstance(vid_writer[i], cv2.VideoWriter):
-                            vid_writer[i].release()  # release previous video writer
-                        if vid_cap:  # video
-                            fps = vid_cap.get(cv2.CAP_PROP_FPS)
-                            w = int(vid_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-                            h = int(vid_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-                        else:  # stream
-                            fps, w, h = 30, im0.shape[1], im0.shape[0]
-                            save_path += '.mp4'
-                        vid_writer[i] = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, (w, h))
-                    vid_writer[i].write(im0)
+
+            
+            # Second-stage classifier (optional)
+            if classify:
+                pred = apply_classifier(pred, modelc, img, im0s)
+
+            # Process predictions
+            for i, det in enumerate(pred):  # detections per image
+                if webcam:  # batch_size >= 1
+                    p, s, im0, frame = path[i], f'{i}: ', im0s[i].copy(), dataset.count
+                else:
+                    p, s, im0, frame = path, '', im0s.copy(), getattr(dataset, 'frame', 0)
+
+                
+
+                p = Path(p)  # to Path
+                save_path = str(save_dir / p.name)  # img.jpg
+                txt_path = str(save_dir / 'labels' / p.stem) + ('' if dataset.mode == 'image' else f'_{frame}')  # img.txt
+                s += '%gx%g ' % img.shape[2:]  # print string
+                gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
+                imc = im0.copy() if save_crop else im0  # for save_crop
+                if len(det):
+                    # Rescale boxes from img_size to im0 size
+                    det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
+
+                    # Print results
+                    for c in det[:, -1].unique():
+                        n = (det[:, -1] == c).sum()  # detections per class
+                        s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
+
+                    # Write results
+                    for *xyxy, conf, cls in reversed(det):
+                        if save_txt:  # Write to file
+                            xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
+                            line = (cls, *xywh, conf) if save_conf else (cls, *xywh)  # label format
+                            with open(txt_path + '.txt', 'a') as f:
+                                f.write(('%g ' * len(line)).rstrip() % line + '\n')
+
+                        if save_img or save_crop or view_img:  # Add bbox to image
+                            c = int(cls)  # integer class
+                            label = None if hide_labels else (names[c] if hide_conf else f'{names[c]} {conf:.2f}')
+                            im0 = plot_one_box(xyxy, im0, label=label, color=colors(c, True), line_width=line_thickness)
+                            if save_crop:
+                                save_one_box(xyxy, imc, file=save_dir / 'crops' / names[c] / f'{p.stem}.jpg', BGR=True)
+
+                # Print time (inference + NMS)
+                print(f'{s}Done. ({t2 - t1:.3f}s)')
+
+                # Stream results
+                if view_img:
+                    cv2.imshow(str(p), im0)
+                    cv2.waitKey(1)  # 1 millisecond 
+
+                # Save results (image with detections) (데이터 저장하고싶으면 아래코드활성화)
+                if save_img:
+                    if dataset.mode == 'image':
+                        cv2.imwrite(save_path, im0)
+                    else:  # 'video' or 'stream'
+                        if vid_path[i] != save_path:  # new video
+                            vid_path[i] = save_path
+                            if isinstance(vid_writer[i], cv2.VideoWriter):
+                                vid_writer[i].release()  # release previous video writer
+                            if vid_cap:  # video
+                                fps = vid_cap.get(cv2.CAP_PROP_FPS)
+                                print("여기가 무슨 fps인지 확인 :", fps, type(fps))
+                                w = int(vid_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+                                h = int(vid_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+                            else:  # stream
+                                fps, w, h = 30, im0.shape[1], im0.shape[0] 
+                                save_path += '.mp4'
+                            print("두번째 fps인지 확인 :", fps, type(fps))
+                            vid_writer[i] = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, (w, h))
+                        
+                        vid_writer[i].write(im0)
+
+        else:
+            print("pass")
 
     if save_txt or save_img:
         s = f"\n{len(list(save_dir.glob('labels/*.txt')))} labels saved to {save_dir / 'labels'}" if save_txt else ''
@@ -289,21 +307,30 @@ def run(weights='yolov5s.pt',  # model.pt path(s)
     if update:
         strip_optimizer(weights)  # update model (to fix SourceChangeWarning)
 
-
     #주영 json 형식으로 저장하기위해 추가한식
-    print(data)
-    log_json = json.dumps(data)
-    with open("log/image_log.json", "w", encoding="utf-8-sig") as json_file:
+    json_name = os.path.basename(source)
+    json_name = os.path.splitext(json_name)[0]
+
+    with open(f"log/{json_name}.json", "w", encoding="utf-8-sig") as json_file:
         json.dump(data, json_file, ensure_ascii=False)
 
 
+    result_name = f"{json_name}.mp4"
+    move_dog(json_name)
+    Highlight(path, result_name, json_name)
+
+
+
+
     print(f'Done. ({time.time() - t0:.3f}s)')
+
+    
 
 
 def parse_opt():
     parser = argparse.ArgumentParser()
     parser.add_argument('--weights', nargs='+', type=str, default='yolov5s.pt', help='model.pt path(s)')
-    parser.add_argument('--source', type=str, default='data/images', help='file/dir/URL/glob, 0 for webcam')
+    parser.add_argument('--source', type=str, default='data/videos/dog_sample.mp4', help='file/dir/URL/glob, 0 for webcam')
     parser.add_argument('--imgsz', '--img', '--img-size', nargs='+', type=int, default=[640], help='inference size h,w')
     parser.add_argument('--conf-thres', type=float, default=0.25, help='confidence threshold')
     parser.add_argument('--iou-thres', type=float, default=0.45, help='NMS IoU threshold')
